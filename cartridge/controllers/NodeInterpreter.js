e4e06410e2c4e3990461f2d4f6b543004b73f87b
David Bertoldi <dbertoldi@salesforce.com>
2024-06-03 10:48:24 +0200

    restricted to dev/sbs environments

--- cartridge/controllers/NodeInterpreter.js
+++ cartridge/controllers/NodeInterpreter.js
@@ -1,33 +1,37 @@
 'use strict';
 
-const Logger = require('dw/system/Logger').getLogger('BMNI','NodeInterpreter');
-const StringUtils = require('dw/util/StringUtils');
+const Logger = require('dw/system/Logger').getLogger('BMNI', 'NodeInterpreter');
 
 // The buffer is a global object.
-var __internal__interpreter__buffer = []
+let internalInterpreterBuffer = [];
 
 /**
  * Shows the editor in the Business Manager
  */
-function __internal__interpreter__show() {
-  dw.template.ISML.renderTemplate('sf/interpreter', {
-    code: '\'use strict\';\n\nvar Site = require(\'dw/system/Site\');\nprint(Site.current.name);',
+function internalInterpreterShow() {
+  const ISML = require('dw/template/ISML');
+  const System = require('dw/system/System');
+  ISML.renderTemplate('sf/interpreter', {
+    enabled: System.instanceType === System.DEVELOPMENT_SYSTEM,
+    code: "'use strict';\n\nvar Site = require('dw/system/Site');\nprint(Site.current.name);",
   });
 }
 
 /**
  * Runs any Node script within a new scope.
  */
-function __internal__interpreter__run() {
-  var code = request.httpParameterMap.code.stringValue;
-  var error = false;
-  var exception = null;
-  var codeResult = null;
+function internalInterpreterRun() {
+  const code = request.httpParameterMap.code.stringValue;
+  let error = false;
+  let exception = null;
+  let codeResult = null;
 
   try {
-    var fn = new Function(code);
+    // eslint-disable-next-line no-new-func
+    const fn = new Function(code);
+    Logger.warn('[BEFORE] Code executed:\n{0}', code);
     codeResult = fn.call(null);
-    Logger.warn('Code executed:\n{0}', code);
+    Logger.warn('[AFTER] Code executed:\n{0}', code);
   } catch (e) {
     exception = e;
     Logger.error('Error in running code:\n{0}', code, e);
@@ -37,42 +41,46 @@ function __internal__interpreter__run() {
 
   response.setBuffered(false);
   response.setContentType('application/json');
-  response.writer.print(JSON.stringify({
-    error: error,
-    exception: exception,
-    codeResult: codeResult,
-    buffer: __internal__interpreter__buffer
-  }));
+  response.writer.print(
+    JSON.stringify({
+      error: error,
+      exception: exception,
+      codeResult: codeResult,
+      buffer: internalInterpreterBuffer,
+    })
+  );
 
-  __internal__interpreter__buffer = [];
+  internalInterpreterBuffer = [];
 }
 
 /**
  * Stores in the buffer object the string that the user wants to print as stdout.
  */
 function print() {
-  __internal__interpreter__out(arguments, false);
+  internalInterpreterOut(arguments, false);
 }
 
 /**
  * Stores in the buffer object the string that the user wants to print as stderr.
  */
 function err() {
-  __internal__interpreter__out(arguments, true);
+  internalInterpreterOut(arguments, true);
 }
 
-function __internal__interpreter__out(args, error) {
-  if (args[0] === null) {
-    args[0] = 'null';
+function internalInterpreterOut(args, error) {
+  const newArgs = args;
+  const StringUtils = require('dw/util/StringUtils');
+  if (newArgs[0] === null) {
+    newArgs[0] = 'null';
   }
-  __internal__interpreter__buffer.push({
+  internalInterpreterBuffer.push({
     error: error,
-    msg: StringUtils.format.apply(null, args)
+    msg: StringUtils.format.apply(null, newArgs),
   });
 }
 
-__internal__interpreter__show.public = true;
-__internal__interpreter__run.public = true;
+internalInterpreterShow.public = true;
+internalInterpreterRun.public = true;
 
-exports.Show = __internal__interpreter__show;
-exports.Run = __internal__interpreter__run;
+exports.Show = internalInterpreterShow;
+exports.Run = internalInterpreterRun;
